080e2aca4c9b9096bef12f529c5150b35fa58b8c
Tacarrem23 <carremitchell729@gmail.com>
2025-07-07 14:59:55 -0400

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -20,3 +20,31 @@ CREATE TABLE playlists_tracks (
   track_id integer NOT NULL REFERENCES tracks(id) ON DELETE CASCADE,
   UNIQUE (playlist_id, track_id)
 );
+
+-- schema.sql
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE tracks (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  duration_ms INTEGER NOT NULL
+);
+
+CREATE TABLE playlists (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT NOT NULL,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE playlists_tracks (
+  id SERIAL PRIMARY KEY,
+  playlist_id INTEGER NOT NULL REFERENCES playlists(id),
+  track_id INTEGER NOT NULL REFERENCES tracks(id),
+  UNIQUE (playlist_id, track_id)
+);
